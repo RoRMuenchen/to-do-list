50d0ddf5277bf0423350cdf9bf3165cea02e92e0
MarMarMet <metschke.martina@gmail.com>
2017-07-19 20:25:24 +0200

    Add Toggle route + button to change todo states

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   resources :rewards
-  resources :tasks
+  resources :tasks do
+    member do
+      post 'toggle'
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
